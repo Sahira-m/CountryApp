d01dc19e6a279763c73403ca356cdcecf25538cb
Sahira-m <sahikmh@gmail.com>
2023-01-12 19:49:27 +0100

    done

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 //Components
 import Search from "../../component/search-component/search";
-import CountryList from "../../component/country-list/CountryList";
+import CountryList from "../../component/country-list/countryList";
  import "./Home.css";
 export function Home(  )
 { 
